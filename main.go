4f4a762bd5cb295ce1b37daad25b3a0024ec1451
ismail118 <ismailalfiyasin6@gmail.com>
2025-06-23 18:38:17 +0700

    add mysql driver

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+const (
+	DBSOURCE = "mysql://root:password@tcp(localhost:3306)/booklending?tls=false"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	dbConn, err := sql.Open("mysql", DBSOURCE)
+	if err != nil {
+		log.Fatalf("Error cannot connect to database: %v", err)
+	}
+	defer dbConn.Close()
+
+	fmt.Println("test connect db")
 }
